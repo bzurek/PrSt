7df2b794f2a1306681de12643e128a627d99027b
Barbara Zurek <barbara.zurek999@gmail.com>
2021-05-13 13:49:19 +0200

    dodanie zadan z 6 rozdzialu

---
+++ zad6.2.11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int suma(int tab [][], int a)
+{
+    int wynik;
+    for(int i = 0, i<a; i++)
+    {
+        for(int j= 0 j<100; j++)
+        {
+            wynik+= tab [i][j]
+        }
+    }
+
+    return wynik;
+}
---
+++ zad6.2.12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int suma(int tab [][], int n, int m)
+{
+    int wynik;
+    for(int i = 0, i<n; i++)
+    {
+        for(int j= 0 j<m; j++)
+        {
+            wynik+= tab[i][j]
+        }
+    }
+
+    return wynik;
+}
+
